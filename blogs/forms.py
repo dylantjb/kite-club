93b112b69d8baa48b3e711da27ec0830068079c0
Timman47 <78560693+Timman47@users.noreply.github.com>
2022-12-02 15:16:32 +0000

    Tests Folder pull (#3)
    * Sign Up Form completed
    
    Implemented sign up form
    
    * Created Test Folder and SignUp tests
    
    Started implementation of sign up form tests

--- blogs/forms.py
+++ blogs/forms.py
@@ -3,4 +3,37 @@ from django.core.validators import RegexValidator
 from .models import User
 
 class SignUpForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'bio']
+        widgets = {'bio': forms.TextArea()}
+
+    new_password = forms.CharField(
+        label = 'Password',
+        widget = forms.PasswordInput(),
+        validators = [RegexValidator(
+            regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message = 'Password must contain an uppercase character, a lowercase character and a number.'
+        )]
+    )
+
+    password_confirmation = forms.CharField(label = 'Password confirmation', widget = forms.PasswordInput())
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    def save(self):
+        super().save(commit = False):
+        user = User.objects.create_user(
+            self.cleaned_data.get('username'),
+            first_name = self.cleaned_data.get('first_name')
+            last_name = self.cleaned_data.get('last_name')
+            email = self.cleaned_data.get('email')
+            bio = self.cleaned_data.get('bio')
+            password = self.cleaned_data.get('new_password')
+        )
+        return user 
---
+++ blogs/tests/test_sign_up_form.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.hashers import check_password
+from django import forms
+from django.test import TestCase
+from blogs.forms import SignUpForm
+from blogs.models import User
+
+class SignUpFormTestCase(TestCase):
+    pass
